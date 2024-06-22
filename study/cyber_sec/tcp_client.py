38327a0adc0116629fb16d21a94910d71e4310ba
tk-955990 <105995713+tk-955990@users.noreply.github.com>
2024-06-22 22:57:20 +0900

    2024/06/22/2257

--- study/cyber_sec/tcp_client.py
+++ study/cyber_sec/tcp_client.py
@@ -1,17 +1,19 @@
 import socket
 
-target_host = '127.0.0.1'
+target_host = "www.google.com"
+target_port = 80
 
-target_port = 9998 
-
+# ソケットオブジェクトの作成
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+# サーバーへ接続
 client.connect((target_host, target_port))
 
-client.send(b"Hello Server!")
+# データの送信
+client.send(b"GET / HTTP/1.1\r\nHost: google.com\r\n\r\n")
 
+# データの受信
 response = client.recv(4096)
 
 print(response.decode())
 client.close()
-
